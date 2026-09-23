f2661b2bd43d35b50f452830ca82abf33b4f4bda
donnyboy72 <donavanwatley@yahoo.com>
2026-09-22 20:41:42 -0400

    built the 'application factory' this is what starts the server. Also made db.py file this will be where we call the databases

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,30 @@
-import flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+#from db import initialize_database
+
+
+def create_app():
+    app = Flask(__name__)
+
+    #app.config["DATABASE"] = database/homefield.db
+
+    # if test_config:
+    #     app.config.update(test_config)
+
+    CORS(
+        app,
+        resources={
+            r"/api/*": {
+                "origins": "http://localhost:5173",
+            }
+        },
+    )
+
+    #initialize_database(app)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
