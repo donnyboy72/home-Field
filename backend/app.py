442ae3307967265dab81c56babe939e25a3d52c5
donnyboy72 <donavanwatley@yahoo.com>
2026-09-22 20:45:03 -0400

    Added commits to explain the code in app.py

--- backend/app.py
+++ backend/app.py
@@ -5,16 +5,22 @@ from flask_cors import CORS
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__) #create a flash app instance
 
     #app.config["DATABASE"] = database/homefield.db
 
     # if test_config:
     #     app.config.update(test_config)
 
+
+
+    # enable CORS for the app
+    # CORS will allow vue and flask communicate with each other. 
+    # They use different ports so this allows communication between the two.
     CORS(
         app,
         resources={
+            #this is what creates the api call for vue to access the backend.
             r"/api/*": {
                 "origins": "http://localhost:5173",
             }
